28eaedde5ffa5122471f65ef51a5e03cccf5ff44
Yuki-takuya-kun <2473098355@qq.com>
2025-04-01 16:59:12 +0800

    chor: rename fields

--- src/main/java/io/github/admiral/common/Trace.java
+++ src/main/java/io/github/admiral/common/Trace.java
@@ -9,7 +9,7 @@ import java.util.concurrent.ConcurrentHashMap;
 /** A structure that conserve tracing data.*/
 public class Trace {
     /** Count how many troop before given troop have not been executed in execution graph.*/
-    public Map<Troop, Integer> watiingTroop = new ConcurrentHashMap<Troop, Integer>();
+    public Map<Troop, Integer> waitingTroops = new ConcurrentHashMap<Troop, Integer>();
 
     /** Reserve how many troop is running.*/
     public Set<Troop> runningTroops = ConcurrentHashMap.newKeySet();
