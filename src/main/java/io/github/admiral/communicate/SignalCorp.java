d119a9d31f768d96b90d209184fa5916294982cf
Yuki-takuya-kun <2473098355@qq.com>
2025-04-01 11:17:32 +0800

    feat: change interface format

--- src/main/java/io/github/admiral/communicate/SignalCorp.java
+++ src/main/java/io/github/admiral/communicate/SignalCorp.java
@@ -3,10 +3,10 @@ package io.github.admiral.communicate;
 /***
  * Interface of communication between different parts.
  */
-public interface SignalCorp {
+public abstract class SignalCorp<R, S> {
     /** Receive message from other signal corp.*/
-    void receive(Message message);
+    public abstract void receive(R message);
 
     /** Send message to other signal corp.*/
-    boolean send(Message message);
+    public abstract void send(S message);
 }
