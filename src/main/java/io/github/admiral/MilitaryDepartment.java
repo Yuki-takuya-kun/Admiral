0814438eb336c5583f1de56208854d542163ed30
Yuki-takuya-kun <2473098355@qq.com>
2025-04-01 11:16:52 +0800

    feat: implement grpc service and client at military department.

--- src/main/java/io/github/admiral/MilitaryDepartment.java
+++ src/main/java/io/github/admiral/MilitaryDepartment.java
@@ -1,15 +1,26 @@
 package io.github.admiral;
 
+import com.google.protobuf.Empty;
+import io.github.admiral.communicate.CommandSignalCorp;
+import io.github.admiral.communicate.TaskAssignGrpc;
+import io.github.admiral.communicate.exchange.ExchangeDataGrpc;
+import io.github.admiral.communicate.exchange.ExchangeSignalCorp;
+import io.github.admiral.communicate.report.ReportSignalCorp;
+import io.github.admiral.communicate.report.TaskReportGrpc;
 import io.github.admiral.service.HumanResource;
 import io.github.admiral.soldier.SoldierCreatable;
 import io.github.admiral.soldier.SoldierInfo;
 import io.github.admiral.soldier.SoldierInstance;
+import io.github.admiral.utils.GrpcChannelPool;
+import io.grpc.ManagedChannel;
+import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import net.devh.boot.grpc.server.service.GrpcService;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,15 +32,19 @@ public class MilitaryDepartment {
     private final ApplicationContext applicationContext;
     private final SoldierCreatable[] soldierFactories;
     private final HumanResource humanResource;
+    private final GrpcChannelPool grpcChannelPool;
 
     private Map<SoldierInfo, SoldierInstance> soldiers = new HashMap<SoldierInfo, SoldierInstance>();
 
     @Autowired
     public MilitaryDepartment(ApplicationContext context,
-                              HumanResource humanResource) {
+                              HumanResource humanResource,
+                              GrpcChannelPool grpcChannelPool) {
         this.applicationContext = context;
         this.humanResource = humanResource;
+        this.grpcChannelPool = grpcChannelPool;
         soldierFactories = applicationContext.getBeansOfType(SoldierCreatable.class).values().toArray(new SoldierCreatable[0]);
+
     }
 
 
@@ -47,12 +62,64 @@ public class MilitaryDepartment {
         registerSoldiers();
     }
 
-    /** Register all soldiers to service center.*/
+    /** Register all soldiers to service center and admiral.*/
     public void registerSoldiers(){
         for (SoldierInfo soldierInfo : soldiers.keySet()) {
             if (!humanResource.register(soldierInfo)){
                 log.error("Soldier " + soldierInfo + "registered fail.");
             };
+            if (soldierInfo.getConsumes().length == 0){
+//                RegisterMessage registerMessage = new RegisterMessage(soldierInfo.getName(), new String[]{"all"});
+//                admiralSignalCorp.send(registerMessage);
+            }
+        }
+    }
+
+    public void report(String host, int port, ReportSignalCorp.ReportMessage reportMessage){
+        ManagedChannel channel = null;
+        try {
+            channel = grpcChannelPool.getChannel(host, port);
+            TaskReportGrpc.TaskReportStub stub = TaskReportGrpc.newStub(channel);
+            // do nothing
+            stub.report(reportMessage, new StreamObserver<Empty>() {
+                @Override
+                public void onNext(Empty empty) {}
+
+                @Override
+                public void onError(Throwable throwable) {}
+
+                @Override
+                public void onCompleted() {}
+            });
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        } finally {
+            if (channel != null) {
+                try {
+                    grpcChannelPool.returnChannel(host, port, channel);
+                } catch (Exception e) {
+                    log.error("return channel failed. \n{}", e.getMessage());
+                }
+            }
+        }
+    }
+
+    @GrpcService
+    public class TaskExecutor extends TaskAssignGrpc.TaskAssignImplBase {
+
+        @Override
+        public void assign(CommandSignalCorp.CommandMessage request,
+                           StreamObserver<com.google.protobuf.Empty> responseObserver){
+            responseObserver.onNext(Empty.getDefaultInstance());
+            responseObserver.onCompleted();
+        }
+    }
+
+    @GrpcService
+    public class GetData extends ExchangeDataGrpc.ExchangeDataImplBase{
+        @Override
+        public void getData(ExchangeSignalCorp.Request request, StreamObserver<ExchangeSignalCorp.Response> response){
+
         }
     }
 
