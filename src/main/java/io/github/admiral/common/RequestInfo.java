9b1cea48653d4c4b826bd777dab4353b2aab3601
Yuki-takuya-kun <2473098355@qq.com>
2025-04-02 21:18:10 +0800

    feat: add fields and constructor for RequestInfo

--- src/main/java/io/github/admiral/common/RequestInfo.java
+++ src/main/java/io/github/admiral/common/RequestInfo.java
@@ -2,9 +2,16 @@ package io.github.admiral.common;
 
 import lombok.Getter;
 
+/** A structure that conserve request information.*/
 @Getter
 public class RequestInfo {
     String requestId;
+    Long createTime;
+
+    public RequestInfo(String requestId, Long createTime) {
+        this.requestId = requestId;
+        this.createTime = createTime;
+    }
 
     @Override
     public int hashCode(){
