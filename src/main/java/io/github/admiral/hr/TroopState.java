4d7b2508de376f61112e2da86beda6835566e4d9
Yuki-takuya-kun <2473098355@qq.com>
2025-04-07 00:31:57 +0800

    feat: enum class reserving troop state.

---
+++ src/main/java/io/github/admiral/hr/TroopState.java
@@ -0,0 +1,14 @@
+package io.github.admiral.hr;
+
+/** A enum class that signify the troop states.
+ * @author Jiahao Hwang
+ * */
+public enum TroopState {
+    /**The troop is creating.*/
+    CREATING,
+    /** The troop is running and can be scheduled.*/
+    ONLINE,
+    /** The troop is offline.*/
+    OFFLINE,
+
+}
